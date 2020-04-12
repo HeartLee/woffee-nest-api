d20fbf90b750c2f6e3e41e4e5ce3f2192af82ce6
Jose Lee <lg282923166@gmail.com>
2020-04-12 17:20:28 +0800

    feat: add dto and params

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { Controller, Get, Post, Body, Query, Param } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiProperty } from '@nestjs/swagger';
+
+class CreatePostDto {
+  //Dto 数据传输对象
+  @ApiProperty({ description: '帖子标题' })
+  title: string;
+  @ApiProperty({ description: '帖子内容' })
+  content: string;
+}
 
 @Controller('posts')
 @ApiTags('博客')
@@ -10,9 +18,11 @@ export class PostsController {
     return [{ id: 1 }];
   }
   @Post()
-  create() {
+  @ApiOperation({ summary: '创建帖子' })
+  create(@Body() body: CreatePostDto, @Query() query, @Param() params) {
     return {
       success: true,
+      data: body,
     };
   }
   @Get(':id')
