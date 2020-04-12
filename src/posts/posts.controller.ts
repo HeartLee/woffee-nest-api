1e94975e8df15dcb232c43b70fe1796d2f9d8b17
Jose Lee <lg282923166@gmail.com>
2020-04-12 17:36:29 +0800

    feat: complete posts api

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Query, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Query,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiProperty } from '@nestjs/swagger';
 
 class CreatePostDto {
@@ -26,10 +35,26 @@ export class PostsController {
     };
   }
   @Get(':id')
-  detail() {
+  @ApiOperation({ summary: '帖子详情' })
+  detail(@Param('id') id: string) {
     return {
-      id: 1,
+      id,
       title: 'woffee 11',
     };
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: '编辑帖子' })
+  update(@Param('id') id: string, @Body() body: CreatePostDto) {
+    return {
+      success: true,
+      data: { id, ...body },
+    };
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: '删除帖子' })
+  remove(@Param('id') id: string) {
+    return { success: true };
   }
 }
